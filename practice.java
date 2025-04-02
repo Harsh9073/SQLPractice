977f928fa8eddf9885fb92eafdcaa226376fcf81
Harsh Anand <harshanand9073@gmail.com>
2025-04-02 23:53:42 +0530

    Update practice.java

--- practice.java
+++ practice.java
@@ -207,4 +207,264 @@ public void delete_data(String sl_no){
 }
 
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 
+public class MongoDBConnectionExample {
+    
+    private static final String URI = "mongodb://localhost:27017";
+    
+    public static void main(String[] args) {
+        // Connect to MongoDB server
+        try (MongoClient mongoClient = MongoClients.create(URI)) {
+            
+            // Access the database
+            MongoDatabase database = mongoClient.getDatabase("mydb");
+            
+            System.out.println("Connected to MongoDB database: " + database.getName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
+public class MongoDBCreateExample {
+
+    public static void main(String[] args) {
+        try (MongoClient mongoClient = MongoClients.create(URI)) {
+            
+            MongoDatabase database = mongoClient.getDatabase("mydb");
+            MongoCollection<Document> collection = database.getCollection("employees");
+
+            // Create a new document
+            Document employee = new Document("name", "Raj")
+                                .append("age", 30)
+                                .append("department", "IT");
+            
+            // Insert the document
+            collection.insertOne(employee);
+            
+            System.out.println("Document inserted: " + employee.toJson());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+try (MongoClient mongoClient = MongoClients.create(URI)) {
+            
+            MongoDatabase database = mongoClient.getDatabase("mydb");
+            MongoCollection<Document> collection = database.getCollection("employees");
+
+            // Update the department of an employee with name "Raj"
+            collection.updateOne(Filters.eq("name", "Raj"),
+                                 Updates.set("department", "Engineering"));
+            
+            System.out.println("Document updated successfully.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+package mongodb_CURD;
+
+import org.bson.Document;
+
+
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+
+public class MongoDBConnectionExampleII {
+	
+	private static final String URI = "mongodb://localhost:27017";
+
+	public static void main(String[] args) {
+		try (com.mongodb.client.MongoClient mongoClient = MongoClients.create(URI)) {
+		            
+		            MongoDatabase database = mongoClient.getDatabase("practice1");
+		            MongoCollection<Document> collection = database.getCollection("products");
+		
+		            // Update the department of an employee with name "Raj"
+		            collection.updateMany(Filters.eq("price", 799),
+		                                 Updates.set("price", 899));
+		            
+		            System.out.println("Document updated successfully.");
+		        } catch (Exception e) {
+		            e.printStackTrace();
+		        }
+			}
+}
+
+package mongodb_CURD;
+
+import org.bson.Document;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+
+public class MongoDBConnectionExampleIII {
+
+    private static final String URI = "mongodb://localhost:27017";
+
+    public static void main(String[] args) {
+        try (MongoClient mongoClient = MongoClients.create(URI)) {
+
+            MongoDatabase database = mongoClient.getDatabase("practice1");
+            MongoCollection<Document> collection = database.getCollection("products");
+
+            
+            System.out.println("All Documents:");
+            try (MongoCursor<Document> cursor = collection.find().iterator()) {
+                while (cursor.hasNext()) {
+                    System.out.println(cursor.next().toJson());
+                }
+            }
+
+            
+            int targetPrice = 899;
+            System.out.println("\nDocuments with price " + targetPrice + ":");
+            try (MongoCursor<Document> cursor = collection.find(Filters.eq("price", targetPrice)).iterator()) {
+                while (cursor.hasNext()) {
+                    System.out.println(cursor.next().toJson());
+                }
+            }
+
+            
+            int minPrice = 800;
+            System.out.println("\nDocuments with price greater than " + minPrice + ":");
+            try (MongoCursor<Document> cursor = collection.find(Filters.gt("price", minPrice)).iterator()) {
+                while (cursor.hasNext()) {
+                    System.out.println(cursor.next().toJson());
+                }
+            }
+
+           
+            String targetName = "xPhone";
+            System.out.println("\nDocuments with name " + targetName + ":");
+            try(MongoCursor<Document> cursor = collection.find(Filters.eq("name", targetName)).iterator()){
+                while(cursor.hasNext()){
+                    System.out.println(cursor.next().toJson());
+                }
+            }
+            
+            System.out.println("Odd-Indexed Products:");
+            try (MongoCursor<Document> cursor = collection.find().iterator()) {
+                int index = 0;
+                while (cursor.hasNext()) {
+                    Document document = cursor.next();
+                    if (index % 2 == 0) { 
+                        System.out.println(document.toJson());
+                    }
+                    index++;
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+package mongodb_CURD;
+
+import org.bson.Document;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+
+public class MongoDBConnectionExampleV {
+
+    private static final String URI = "mongodb://localhost:27017";
+
+    public static void main(String[] args) {
+        try (MongoClient mongoClient = MongoClients.create(URI)) {
+
+            MongoDatabase database = mongoClient.getDatabase("practice1");
+            MongoCollection<Document> collection = database.getCollection("products");
+
+           
+            Document deletedDocument = collection.findOneAndDelete(Filters.eq("name", "xPhone")); // Replace "Specific Product"
+
+            if (deletedDocument != null) {
+                System.out.println("Deleted document: " + deletedDocument.toJson());
+            } else {
+                System.out.println("Document with the specified name not found.");
+            }
+
+            
+//            long deletedCount = collection.deleteMany(Filters.gt("price", 1000)).getDeletedCount(); //delete all products with price greater than 1000
+//            System.out.println("Deleted " + deletedCount + " documents.");
+
+            // caution ahead!
+            //long allDeletedCount = collection.deleteMany(new Document()).getDeletedCount();
+            //System.out.println("Deleted all " + allDeletedCount + " documents in the collection.");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+package mongodb_CURD;
+
+import org.bson.Document;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+
+public class MongoDBConnectionExampleVI {
+
+    private static final String URI = "mongodb://localhost:27017";
+
+    public static void main(String[] args) {
+        try (MongoClient mongoClient = MongoClients.create(URI)) {
+
+            MongoDatabase database = mongoClient.getDatabase("practice1");
+            MongoCollection<Document> collection = database.getCollection("products");
+
+        
+            System.out.println("Documents with 'description' field:");
+            try (MongoCursor<Document> cursor = collection.find(Filters.exists("spec")).iterator()) {
+                while (cursor.hasNext()) {
+                    System.out.println(cursor.next().toJson());
+                }
+            }
+
+            
+            System.out.println("\nDocuments where 'price' is a int:");
+            try (MongoCursor<Document> cursor = collection.find(Filters.type("price", "int")).iterator()) {
+                while (cursor.hasNext()) {
+                    System.out.println(cursor.next().toJson());
+                }
+            }
+
+            
+            System.out.println("\nDocuments where 'tags' array has size 3:");
+            try (MongoCursor<Document> cursor = collection.find(Filters.size("color", 3)).iterator()) {
+                while (cursor.hasNext()) {
+                    System.out.println(cursor.next().toJson());
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
